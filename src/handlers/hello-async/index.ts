a90ab21bca835406def045b3a7319f33b638a64f
Kurt <breath103@gmail.com>
2018-03-26 14:01:03 +0900

    update templates

--- src/handlers/hello-async/index.ts
+++ src/handlers/hello-async/index.ts
@@ -1,32 +1,23 @@
 import * as LambdaProxy from "../../interfaces/lambda-proxy";
 
-import raven from "../../helpers/raven-helper";
-
 export default async function handler(event: LambdaProxy.Event) {
-  raven.setContext({ event });
+  const response = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        statusCode: 200,
+        headers: {
+          "Content-Type": "text/html",
+        },
+        body: `
+          <html>
+            <body>
+              <h1> TITLE </h1>
+            </body>
+          </html>
+        `,
+      });
+    }, 1);
+  });
 
-  try {
-    const response = await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          statusCode: 200,
-          headers: {
-            "Content-Type": "text/html",
-          },
-          body: `
-            <html>
-              <body>
-                <h1> TITLE </h1>
-              </body>
-            </html>
-          `,
-        });
-      }, 1);
-    });
-
-    return response;
-  } catch (e) {
-    await raven.capture(e);
-    return null;
-  }
+  return response;
 }
