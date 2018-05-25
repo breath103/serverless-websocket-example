faab5a1a3766ee1be01132c92efe3cb4b0a99234
Kurt <breath103@gmail.com>
2018-05-25 11:05:46 +0900

    update package.json

--- src/handlers/hello/__test__/index_test.ts
+++ src/handlers/hello/__test__/index_test.ts
@@ -1,6 +1,4 @@
 import * as chai from "chai";
-import * as chaiAsPromised from "chai-as-promised";
-chai.use(chaiAsPromised);
 chai.should();
 
 import * as LambdaProxy from "../../../interfaces/lambda-proxy";
