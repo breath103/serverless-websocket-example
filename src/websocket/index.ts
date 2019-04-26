ed09fb165c20e8cda252acf54c4128daaa7dd83c
Kurt Lee <breath103@gmail.com>
2019-04-27 04:28:47 +0900

    P2 checkpoint

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -41,6 +41,11 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     } else {
       // All the other actions
       logger("$default event - %o", event.body);
+
+      const message = JSON.parse(event.body!);
+
+      // Let's echo.
+      await broadcastMessageToClient(message);
     }
   } catch (e) {
     logger("$default Error: %j\n%o", event.body, e);
@@ -55,3 +60,26 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     body: "Success",
   };
 };
+
+
+/**
+ *
+ * Server -> Client Messaging features
+ *
+ */
+import * as AWS from "aws-sdk";
+
+const WebsocketAPIGatewayAddress = "https://42miiaobvk.execute-api.ap-northeast-2.amazonaws.com/prod";
+const apiGateway = new AWS.ApiGatewayManagementApi({ endpoint: WebsocketAPIGatewayAddress });
+
+async function sendMessageToClient(sessionId: string, message: any) {
+  await apiGateway.postToConnection({
+    ConnectionId: sessionId,
+    Data: JSON.stringify(message),
+  }).promise();
+}
+
+async function broadcastMessageToClient(message: any) {
+  const sessions = (await Session.primaryKey.scan({})).records;
+  await Promise.all(sessions.map((record) => sendMessageToClient(record.sessionId, message)));
+}
